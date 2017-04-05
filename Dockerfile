f80e9da903b79446783e47122cb7996a929e6f73
nakamura naoya <nakamura_n2@wm-40ED.local>
2017-04-05 16:13:51 +0900

    update chainer to 1.22.0

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
-FROM nvidia/cuda:8.0-cudnn5-devel-ubuntu16.04
+FROM nvidia/cuda:8.0-cudnn5-devel-ubuntu14.04
 WORKDIR /dockerwork
 
 RUN set -x && apt-get update && apt-get install -y\
     python3\
     python3-pip\
-    && pip3 install chainer==1.21.0\
+    vim\
+    && pip3 install chainer==1.22.0\
 
     # clean 
     && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-COPY chainer_1.21.0_examples/ examples/
+COPY chainer_1.22.0_examples/ examples/
--- chainer_1.22.0_examples/mnist/train_mnist.py
+++ chainer_1.22.0_examples/mnist/train_mnist.py
@@ -1,83 +1,86 @@
+#!/usr/bin/env python
+
 from __future__ import print_function
+
+try:
+    import matplotlib
+    matplotlib.use('Agg')
+except ImportError:
+    pass
+
 import argparse
 
 import chainer
+import chainer.functions as F
 import chainer.links as L
 from chainer import training
 from chainer.training import extensions
 
-from chainer.datasets import get_cifar10
-from chainer.datasets import get_cifar100
 
-import models.VGG
+# Network definition
+class MLP(chainer.Chain):
 
+    def __init__(self, n_units, n_out):
+        super(MLP, self).__init__(
+            # the size of the inputs to each layer will be inferred
+            l1=L.Linear(None, n_units),  # n_in -> n_units
+            l2=L.Linear(None, n_units),  # n_units -> n_units
+            l3=L.Linear(None, n_out),  # n_units -> n_out
+        )
 
-class TestModeEvaluator(extensions.Evaluator):
-
-    def evaluate(self):
-        model = self.get_target('main')
-        model.train = False
-        ret = super(TestModeEvaluator, self).evaluate()
-        model.train = True
-        return ret
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        h2 = F.relu(self.l2(h1))
+        return self.l3(h2)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Chainer CIFAR example:')
-    parser.add_argument('--dataset', '-d', default='cifar10',
-                        help='The dataset to use: cifar10 or cifar100')
-    parser.add_argument('--batchsize', '-b', type=int, default=128,
+    parser = argparse.ArgumentParser(description='Chainer example: MNIST')
+    parser.add_argument('--batchsize', '-b', type=int, default=100,
                         help='Number of images in each mini-batch')
-    parser.add_argument('--epoch', '-e', type=int, default=300,
+    parser.add_argument('--epoch', '-e', type=int, default=20,
                         help='Number of sweeps over the dataset to train')
-    parser.add_argument('--gpu', '-g', type=int, default=0,
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
                         help='GPU ID (negative value indicates CPU)')
     parser.add_argument('--out', '-o', default='result',
                         help='Directory to output the result')
     parser.add_argument('--resume', '-r', default='',
                         help='Resume the training from snapshot')
+    parser.add_argument('--unit', '-u', type=int, default=1000,
+                        help='Number of units')
     args = parser.parse_args()
 
     print('GPU: {}'.format(args.gpu))
+    print('# unit: {}'.format(args.unit))
     print('# Minibatch-size: {}'.format(args.batchsize))
     print('# epoch: {}'.format(args.epoch))
     print('')
 
-    # Set up a neural network to train.
+    # Set up a neural network to train
     # Classifier reports softmax cross entropy loss and accuracy at every
     # iteration, which will be used by the PrintReport extension below.
-    if args.dataset == 'cifar10':
-        print('Using CIFAR10 dataset.')
-        class_labels = 10
-        train, test = get_cifar10()
-    elif args.dataset == 'cifar100':
-        print('Using CIFAR100 dataset.')
-        class_labels = 100
-        train, test = get_cifar100()
-    else:
-        raise RuntimeError('Invalid dataset choice.')
-    model = L.Classifier(models.VGG.VGG(class_labels))
+    model = L.Classifier(MLP(args.unit, 10))
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
         model.to_gpu()  # Copy the model to the GPU
 
-    optimizer = chainer.optimizers.MomentumSGD(0.1)
+    # Setup an optimizer
+    optimizer = chainer.optimizers.Adam()
     optimizer.setup(model)
-    optimizer.add_hook(chainer.optimizer.WeightDecay(5e-4))
+
+    # Load the MNIST dataset
+    train, test = chainer.datasets.get_mnist()
 
     train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
     test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
                                                  repeat=False, shuffle=False)
+
     # Set up a trainer
     updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
     trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
 
     # Evaluate the model with the test dataset for each epoch
-    trainer.extend(TestModeEvaluator(test_iter, model, device=args.gpu))
-
-    # Reduce the learning rate by half every 25 epochs.
-    trainer.extend(extensions.ExponentialShift('lr', 0.5),
-                   trigger=(25, 'epoch'))
+    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
 
     # Dump a computational graph from 'loss' variable at the first iteration
     # The "main" refers to the target link of the "main" optimizer.
@@ -88,6 +91,16 @@ def main():
 
     # Write a log of evaluation statistics for each epoch
     trainer.extend(extensions.LogReport())
+
+    # Save two plot images to the result dir
+    if extensions.PlotReport.available():
+        trainer.extend(
+            extensions.PlotReport(['main/loss', 'validation/main/loss'],
+                                  'epoch', file_name='loss.png'))
+        trainer.extend(
+            extensions.PlotReport(
+                ['main/accuracy', 'validation/main/accuracy'],
+                'epoch', file_name='accuracy.png'))
 
     # Print selected entries of the log to stdout
     # Here "main" refers to the target link of the "main" optimizer again, and
@@ -107,6 +120,7 @@ def main():
 
     # Run the training
     trainer.run()
+
 
 if __name__ == '__main__':
     main()
